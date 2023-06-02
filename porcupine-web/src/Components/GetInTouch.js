6b771f4ccdf2e5ed60297368e07a4ab509f59878
ckriutz <casey.kriutzfield@live.com>
2023-06-02 14:19:26 -0400

    Added some new types!

--- porcupine-web/src/Components/GetInTouch.js
+++ porcupine-web/src/Components/GetInTouch.js
@@ -1,16 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
 import './GetInTouch.css';
 function GetInTouch() {
+    const [GetInTouchData, setData] = useState([])
+    const headers = {
+        'Authorization': 'Bearer ' + process.env.REACT_APP_STRAPI_BEARER
+     };
+    
+    useEffect(() => async() => {
+        const response = await fetch('https://lpunionnc-cms.azurewebsites.net/api/getintouch?populate=*', {method:'GET', headers:headers});
+        const json = await response.json();
+        setData(json.data.attributes);
+    }, []);
+
     return (
         <section>
             <header className="major">
                 <h2>Get in touch</h2>
             </header>
-            <p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+            <p>{GetInTouchData.Details}</p>
             <ul className="contact">
-                <li className="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>
-                <li className="icon solid fa-phone">(000) 000-0000</li>
-                <li className="icon solid fa-home">1234 Somewhere Road #8254<br />
-                Nashville, TN 00000-0000</li>
+                <li className="icon solid fa-envelope"><a href="#">{GetInTouchData.Email}</a></li>
             </ul>
         </section>
     );

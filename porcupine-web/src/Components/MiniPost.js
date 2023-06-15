743da405e87318e45ecd21a5634fbc29e5998319
ckriutz <casey.kriutzfield@live.com>
2023-06-15 17:30:41 -0400

    Added Sidebar minipost.

--- porcupine-web/src/Components/MiniPost.js
+++ porcupine-web/src/Components/MiniPost.js
@@ -2,8 +2,8 @@ import './MiniPost.css';
 function MiniPost(props) {
     return (
         <article>
-            <a href="#" className="image"><img src={props.imgSrc} alt="" /></a>
-            <p>This is a fucking mini-post, yo!</p>
+            <a href={props.link} className="image"><img src={props.imgSrc} alt="" /></a>
+            <p>{props.text}</p>
         </article>
     );
 }
--- porcupine-web/src/Components/Sidebar.js
+++ porcupine-web/src/Components/Sidebar.js
@@ -1,11 +1,42 @@
+import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
 import './Sidebar.css';
 import GetInTouch from './GetInTouch';
 import MiniPost from './MiniPost';
 import Footer from './Footer'
 import Topbox from './Topbox';
 import Menu from './Menu';
+const qs = require('qs');
 
 function Sidebar(props) {
+    const [postData, setPostData] = useState([])
+    
+    const headers = {
+        'Authorization': 'Bearer ' + process.env.REACT_APP_STRAPI_BEARER
+     };
+
+     useEffect(() => {
+        getNewsPosts().then(d => {setPostData(d);});  
+    }, []);
+
+    // Function to collect data
+    async function getNewsPosts() {
+        const query = qs.stringify({
+            pagination: {
+                start: 0,
+                limit: 3,
+            },
+            sort: ['publishedAt:desc'],
+            fields: ['Description', 'Link'],
+            encodeValuesOnly: true, // prettify URL
+        });
+        console.log('https://lpunionnc-cms.azurewebsites.net/api/newses?' + query);
+        const response = await fetch('https://lpunionnc-cms.azurewebsites.net/api/newses?populate=*&' + query, {method:'GET', headers:headers});
+        const json = await response.json();
+        return json.data;
+    };
+
+
     return (
         <div id="sidebar">
 			<div className="inner">
@@ -16,9 +47,9 @@ function Sidebar(props) {
                         <h2>Libertarian News Articles</h2>
                     </header>
                     <div className="mini-posts">
-                        <MiniPost imgSrc="images/pic07.jpg"></MiniPost>
-                        <MiniPost imgSrc="images/pic08.jpg"></MiniPost>
-                        <MiniPost imgSrc="images/pic09.jpg"></MiniPost>
+                        {postData.map((element, index) =>
+                            <MiniPost key={index} link={element.attributes.Link} imgSrc={element.attributes.Image.data.attributes.url} text={element.attributes.Description} />
+                        )}                 
                     </div>
                     <ul className="actions">
                         <li><a href="#" className="button">More</a></li>

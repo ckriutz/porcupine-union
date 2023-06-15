21c263a59391c05df842e8cdeb0a4d7ea2f72e72
ckriutz <casey.kriutzfield@live.com>
2023-06-15 17:37:42 -0400

    Maybe we don't want to run a test.

--- porcupine-web/src/App.test.js
+++ porcupine-web/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Libertarian Party of Union County text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Libertarian Party of Union County/i);
   expect(linkElement).toBeInTheDocument();
 });

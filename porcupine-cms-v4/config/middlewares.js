193fc262144912c121c9de7428a298254607dd19
ckriutz <casey.kriutzfield@live.com>
2023-07-22 18:35:43 -0400

    Added MIddleware for R2

--- porcupine-cms-v4/config/middlewares.js
+++ porcupine-cms-v4/config/middlewares.js
@@ -1,12 +1,26 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({env}) => [
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CF_PUBLIC_ACCESS_URL"),
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CF_PUBLIC_ACCESS_URL"),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
